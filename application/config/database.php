5bc6060d64baf413acbc514b699a8d3a0a352ea3
mubar3 <55468410+mubar3@users.noreply.github.com>
2021-09-25 10:47:02 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -96,3 +96,27 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	// 'username' => 'triptaso_sisako',
+// 	'username' => 'root',
+// 	// 'password' => 'adminsisako',
+// 	'password' => '',
+// 	'database' => 'triptaso_sisako',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
