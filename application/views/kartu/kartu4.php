e76c7fccda815a903c699150fbe2b680fab53101
mubar3 <55468410+mubar3@users.noreply.github.com>
2021-11-19 16:51:48 +0700

    fix kartu

--- application/views/kartu/kartu4.php
+++ application/views/kartu/kartu4.php
@@ -33,7 +33,7 @@ body {
   // code...
 ?>
 <!-- <div style="float: left; margin-left: 20px; margin-top:-13px;width: 550px;height: 350px;margin-bottom: 12px;background-size: 550px 350px;background-image: url('<?php echo base_url(); ?>assets/img/kartu/pramuka.png');"> -->
-  <div style=" padding: 25px; float: left; margin-left: 8px; margin-top:-64px;width: 570px;height: 350px;margin-bottom: 12px;background-size: 570px 350px;background-image: url('<?php echo base_url(); ?>assets/img/kartu/pramuka.png'); background-repeat: no-repeat; background-origin: content-box;">
+  <div style=" padding: 25px; float: left; margin-left: 21px; margin-top:-64px;width: 550px;height: 350px;margin-bottom: 12px;background-size: 550px 350px;background-image: url('<?php echo base_url(); ?>assets/img/kartu/pramuka.png'); background-repeat: no-repeat; background-origin: content-box;">
 
   <img style="position: absolute;margin-left: 34px;margin-top: 98px; width: 105px; height: 140px;overflow: hidden;" class="img-responsive img" src="<?php echo base_url(); ?>assets/img/foto/<?php echo $d->image ?>">
 
